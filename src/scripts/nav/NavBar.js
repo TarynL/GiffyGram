8d0458bd12e76360fcdf1747b2c3974017a9ba00
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-03-10 09:33:37 -0600

    adjusting css and updated images

--- src/scripts/nav/NavBar.js
+++ src/scripts/nav/NavBar.js
@@ -2,7 +2,7 @@ export const NavBar = () => {
     return `
         <nav class="navigation">
             <div class="navigation__item navigation__icon">
-                <img src="./scripts/images/pb.png" alt="Giffygram icon" />
+                <img src="./images/pb.png" alt="Giffygram icon" />
             </div>
             <div class="navigation__item navigation__name">
                 Giffygram 
@@ -11,7 +11,7 @@ export const NavBar = () => {
                 <input type="text" id="postSearch" placeholder="Search posts..." />
             </div>
             <div class="navigation__item navigation__message">
-                <img id="directMessageIcon" src="./scripts/images/fountain-pen.jpg" alt="Direct message" />
+                <img id="directMessageIcon" src="./images/fountain-pen.svg" alt="Direct message" />
             </div>
             <div class="navigation__item navigation__logout">
                 <button id="logout" class="fakeLink">Logout</button>
