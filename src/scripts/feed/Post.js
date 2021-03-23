2a0343908d90527bb629c7160de9ab39068a6b6b
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-03-23 11:14:47 -0500

    added delete button

--- src/scripts/feed/Post.js
+++ src/scripts/feed/Post.js
@@ -4,9 +4,12 @@ export const Post = (postObject) => {
         <header>
             <h2 class="post__title">${postObject.title}</h2>
         </header>
-        <img class="post__image" src="${postObject.imageURL}" />
+        <img class="post__image" src="${postObject.imageURL}" alt="${postObject.description}"/>
         <h3 class="post_description"
         <div><button id="Pug Hug--${postObject.id}">Pug Hug Here</button></div>
+        <button id="delete__${postObject.id}">Delete</button>
       </section>
     `
   }
+
+  
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,8 @@
-import {getUsers, getPosts} from "./data/DataManager.js"
+import {getUsers, getPosts, usePostCollection, getLoggedInUser, createPost, deletePost } from "./data/DataManager.js"
 import {PostList} from "./feed/PostList.js"
 import {NavBar} from "./Nav/NavBar.js"
 import {Footer} from "./Nav/Footer.js"
+import {PostEntry} from "./feed/PostEntry.js"
 /**
  * Main logic module for what should happen on initial page load for Giffygram
  */
@@ -29,6 +30,12 @@ const showFooter = () => {
 	footerElement.innerHTML = Footer();
 }
 
+const showPostEntry = () => { 
+  //Get a reference to the location on the DOM where the nav will display
+  const entryElement = document.querySelector(".entryForm");
+  entryElement.innerHTML = PostEntry();
+}
+
 /*
     This function performs one, specific task.
 
@@ -39,6 +46,7 @@ const startGiffyGram = () => {
     showPostList();
     showNavBar();
     showFooter();
+    showPostEntry();
     
 } ;
 // Are you defining the function here or invoking it?
@@ -57,8 +65,24 @@ applicationElement.addEventListener("change", event => {
       const yearAsNumber = parseInt(event.target.value)
   
       console.log(`User wants to see posts since ${yearAsNumber}`)
+      // invoke a filter function passing the year as an argument
+      showFilteredPosts(yearAsNumber);
     }
   })
+
+  const showFilteredPosts = (year) => {
+    //get a copy of the post collection
+    const epoch = Date.parse(`01/01/${year}`);
+    //filter the data
+    const filteredData = usePostCollection().filter(singlePost => {
+      if (singlePost.timestamp >= epoch) {
+        return singlePost
+      }
+    })
+
+    const postElement = document.querySelector(".postList");
+    postElement.innerHTML = PostList(filteredData);
+  }
 
   applicationElement.addEventListener("click", (event) => {
 	
@@ -66,4 +90,47 @@ applicationElement.addEventListener("change", event => {
 		console.log("post clicked", event.target.id.split("--"))
 		console.log("the id is", event.target.id.split("--")[1])
 	}
+})
+
+applicationElement.addEventListener("click", event => {
+  if (event.target.id === "newPost__cancel") {
+      //clear the input fields
+  }
+})
+
+applicationElement.addEventListener("click", event => {
+  event.preventDefault();
+  if (event.target.id === "newPost__submit") {
+  //collect the input values into an object to post to the DB
+    const title = document.querySelector("input[name='postTitle']").value
+    const url = document.querySelector("input[name='postURL']").value
+    const description = document.querySelector("textarea[name='postDescription']").value
+    //we have not created a user yet - for now, we will hard code `1`.
+    //we can add the current time as well
+    const postObject = {
+        title: title,
+        imageURL: url,
+        description: description,
+        userId: getLoggedInUser().id,
+        timestamp: Date.now()
+    }
+
+  // be sure to import from the DataManager
+      createPost(postObject)
+      .then(response =>{
+        showPostList();
+      })
+  }
+})
+
+applicationElement.addEventListener("click", event => {
+  event.preventDefault();
+  if (event.target.id.startsWith("delete")) {
+    const postId = event.target.id.split("__")[1];
+    
+    deletePost(postId)
+      .then(response => {
+        showPostList();
+      })
+  }
 })
